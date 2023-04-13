487ecd1397bb3699d9b827a0070b7710b2a3a48c
Fred Araujo <fredericoaraujo@gmail.com>
2023-04-13 15:55:52 -0300

    Ajustando os imports

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,18 +1,28 @@
 package med.voll.api.controller;
 
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
-import med.voll.api.paciente.*;
-
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.paciente.AlterarDadosPaciente;
+import med.voll.api.paciente.CadastroDadosPaciente;
+import med.voll.api.paciente.DadosDetalhamentoPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
 
 @RestController
 @RequestMapping("pacientes")
